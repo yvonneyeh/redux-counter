581c5a618de16ac9763a8538b4ffde3ec6983895
Yvonne Yeh <code@yvonneyeh.com>
2021-11-24 13:38:22 -0800

    Add getAllUnresolvedIncidents function

--- thunk/index.js
+++ thunk/index.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 /*
 Features:
@@ -17,7 +18,9 @@ To Do:
 
 const initialState = {
     count: 0,
-    running: true
+    running: true,
+    loading: false,
+    data: []
 }
 
 function reducer(state = initialState, action) {
@@ -37,24 +40,65 @@ function reducer(state = initialState, action) {
                 ...state,
                 running: !state.running
             }
+        case "callAPI":
+            return {
+                ...state,
+                loading: true,
+            }
+        case "success":
+            return {
+                ...state,
+                loading: false,
+                data: action.data
+            }
+        case "failure":
+            return {
+                ...state,
+                loading: false,
+                data: []
+            }
         default:
             return state
     }
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, initialState, applyMiddleware(thunk))
+
+function getAllUnresolvedIncidents() {
+    return (dispatch, state) => {
+        dispatch({ type: "callAPI"})
+
+        fetch('https://status.hologram.io/api/v2/incidents/unresolved.json', {
+            method: 'GET'
+        }).then(data => {
+            return data.json()
+        }).then(json => {
+            console.log(json);
+            dispatch({ type: "success", data: json})
+        }).catch(error => {
+            console.log(error)
+            dispatch({ type: "failure", error})
+
+        })
+    }
+}
 
 store.subscribe(render)
 
 function render() {
     const state = store.getState()
     const count = document.querySelector("#count")
+    const data = document.querySelector("#data")
+    const json = JSON.stringify(state.data)
 
     // get the current count and display it
     count.textContent = `Count: ${state.count}`
 
     // show a play button or pause button based on state
     document.querySelector("#play").textContent = state.running ? "⏸" : "▶️"
+
+    data.textContent = `Data: ${json}`
+
 }
 
 setInterval(() => {
@@ -79,5 +123,10 @@ document.querySelector("#minus").addEventListener("click", () => {
 document.querySelector("#play").addEventListener("click", () => {
     // check if the app is paused or not
     // dispatch an action to either play or pause the counter
-    store.dispatch({ type: "toggleRunning" })
+    store.dispatch({ type: "toggleRunning"})
+})
+
+document.querySelector("#api").addEventListener("click", () => {
+    console.log("hello")
+    store.dispatch(getAllUnresolvedIncidents())
 })
